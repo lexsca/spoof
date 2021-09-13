759aa95637045a71d791d72a1c25b08c1d59779c
Lex Scarisbrick <lex@scarisbrick.org>
2021-09-12 18:43:15 -0600

    fixup docs

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 
-import sphinx_readable_theme
+import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath(".."))
 
@@ -78,8 +78,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme_path = [sphinx_readable_theme.get_html_theme_path()]
-html_theme = "readable"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
