fc406f6801d6ce6b726c5a7f999d8303decb12e8
Lex Scarisbrick <lex@scarisbrick.org>
2022-11-06 14:36:30 -0700

    add python 3.11 support

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Internet :: Proxy Servers ",
         "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
         "Topic :: Software Development :: Libraries :: Python Modules",
